e799e87a4b558a7f405c4d901ba70c0e28b5e81c
Zeev <87901027+Zeev-x@users.noreply.github.com>
2023-02-20 17:20:56 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,299 @@
+//Code by Zeev-x
+//Database bot/inti bot
+//jangan jual script ini ya!!!!
+//Recode boleh ngaku-ngaku jangan !!
+//database ini di buat mendadak jadi mohon maaf kalau ada kesalahan 😂
+var zxy = `</center>
+    <style>
+    body {
+      height: 95vh;
+      background-image: url("https://telegra.ph/file/be6e4139db9e8e09ead09.png");
+      background-attachment: fixed;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      color: black;
+      margin: auto;
+    }
+    button {
+      padding: 10px 50px;
+      border: black 3px solid;
+      font-family: Monospace;
+      border-radius: 25px;
+      background-color: #ffffff7c;
+    }
+    h6,h1 {
+      font-size: 250%;
+    }
+  </style>`
+function loli(){
+  var data = ["https://telegra.ph/file/be6e4139db9e8e09ead09.png",
+"https://telegra.ph/file/c24a85f491d9799e145d9.jpg",
+"https://telegra.ph/file/98681af2db3f98e8ecfc5.jpg",
+"https://telegra.ph/file/0dcb42c22560d1f721e5e.jpg",
+"https://telegra.ph/file/08428049d5cc3f957ddc8.jpg",
+"https://telegra.ph/file/d3006c872eaa81f0627d9.jpg",
+"https://telegra.ph/file/cfd5923299ffb5d3f2e4d.jpg",
+"https://telegra.ph/file/be80619dac8f0c441872a.jpg",
+"https://telegra.ph/file/a8d49f434ea169d1d2def.jpg",
+"https://telegra.ph/file/dfc4aff9b78ae74b76667.jpg",
+"https://telegra.ph/file/3ea823ba0a51d0d6ba422.jpg",
+"https://telegra.ph/file/e4c51f28b3bf853edc71e.jpg",
+"https://telegra.ph/file/e4361c34e872fd14c79d9.jpg",
+"https://telegra.ph/file/95a45f95526e3f710b248.jpg",
+"https://telegra.ph/file/e4361c34e872fd14c79d9.jpg",
+"https://telegra.ph/file/f326fd27cfe62a29f9e76.jpg",
+"https://telegra.ph/file/3421ffba6432797eb5924.jpg",
+"https://telegra.ph/file/6d65730c0046604d27707.jpg",
+"https://telegra.ph/file/b735d5f8d192f7af48d99.jpg",
+"https://telegra.ph/file/e23dcb54da2592f09b6aa.jpg",
+"https://telegra.ph/file/7de8e4056ad3acad718d1.jpg",
+"https://telegra.ph/file/1b20374c736a3776d14bd.jpg"];
+  var x = Math.floor(Math.random()*data.length);
+  var result = data[x];
+  var web = "<center><h1>Loli</h1><br>" + `<img src="${result}" height="500px"> <br><h6>${result}</h6></br>` + zxy;
+  document.write(web);
+}
+function hentai(){
+  var i = prompt("Berapa umurmu?");
+  if(i >= 18){
+    alert("Konten sensitif hanya untuk umur 18+");
+  } else {
+    window.return();
+  }
+  var data = ["https://telegra.ph/file/7461f5f0d9b1689a67b22.png",
+"https://telegra.ph/file/1c93d096aeb9cc1444579.jpg",
+"https://telegra.ph/file/6eac8f189094b5e7aa7e1.jpg",
+"https://telegra.ph/file/9cf54dcc3970765ca2132.jpg",
+"https://telegra.ph/file/49c07d70a029588969baf.jpg",
+"https://telegra.ph/file/02b53a3a2a1524c71c6e0.png",
+"https://telegra.ph/file/fd0e27a89d8a41e7e9279.jpg",
+"https://telegra.ph/file/93a74eff179d08bd20220.png",
+"https://telegra.ph/file/81ece9e34131aba44dc13.jpg",
+"https://telegra.ph/file/aa146a69f2f3526ccb655.png",
+"https://telegra.ph/file/866acbffd37203cf51bc9.jpg",
+"https://telegra.ph/file/5bb616d0c424c27ee7ecf.jpg",
+"https://telegra.ph/file/4605b68cdb1f50f2f2f17.png",
+"https://telegra.ph/file/a54b7b50af277e9370c69.jpg",
+"https://telegra.ph/file/406c74a19c8f957cc0980.jpg",
+"https://telegra.ph/file/d1c5a7f806862027173a1.jpg",
+"https://telegra.ph/file/1dc3219f3660bc68d6067.jpg",
+"https://telegra.ph/file/33483811b7c3970c0af36.jpg",
+"https://telegra.ph/file/08660b519d42c43c588fc.jpg",
+"https://telegra.ph/file/9edd6ce63a9dc5682d04f.jpg",
+"https://telegra.ph/file/ec53b71f11bbde53ba5fa.jpg",
+"https://telegra.ph/file/ec53b71f11bbde53ba5fa.jpg",
+"https://telegra.ph/file/53f26c5a63e86b313d4ea.jpg",
+"https://telegra.ph/file/10629fe8c1009d2a2032b.jpg",
+"https://telegra.ph/file/900348d0f8646f9328b65.jpg",
+"https://telegra.ph/file/bf3c6a34850d5b3c257d8.jpg",
+"https://telegra.ph/file/e7a36201ab0f6b00fb453.jpg",
+"https://telegra.ph/file/140a134de9f718e3c7bdf.jpg",
+"https://telegra.ph/file/ccb6315b7d60f19d2a1a7.jpg",
+"https://telegra.ph/file/ea40ca30547defd79083d.jpg",
+"https://telegra.ph/file/dad9614fb7be7c186bc2b.jpg",
+"https://telegra.ph/file/056d0bb1a672120a2bdd1.jpg",
+"https://telegra.ph/file/53a00c39e0ecb94d88454.jpg",
+"https://telegra.ph/file/0c40e151fa5ac5a649e15.jpg",
+"https://telegra.ph/file/9fb553550549f56aec868.jpg",
+"https://telegra.ph/file/39f40422f0fb5ea9b666d.jpg",
+"https://telegra.ph/file/ee0d61cabe39478fd8d27.jpg",
+"https://telegra.ph/file/196203b7c52896a4eb1e2.jpg",
+"https://telegra.ph/file/8c29555c1d5a4e605af50.jpg",
+"https://telegra.ph/file/a2c1203edbc500765c3e9.jpg",
+"https://telegra.ph/file/a1b370636c29ed8764d64.jpg",
+"https://telegra.ph/file/73d6f4e01e5189221834f.png",
+"https://telegra.ph/file/4d7c9f784f80c489e9593.jpg",
+"https://telegra.ph/file/9d5a1e2b3b02867107f14.jpg",
+"https://telegra.ph/file/b0e0a9fae5bf5689de178.jpg",
+"https://telegra.ph/file/b0e0a9fae5bf5689de178.jpg",
+"https://telegra.ph/file/f6af1e37448bc46484e10.jpg",
+"https://telegra.ph/file/69f48a94518cc736fe5b5.jpg",
+"https://telegra.ph/file/8d890acd9e954335b4d6e.jpg",
+"https://telegra.ph/file/035fd784fb5f667155dd2.jpg"];
+  var x = Math.floor(Math.random()*data.length);
+  var result = data[x];
+  var web = "<center><h1>Hentai</h1><br>" + `<img src="${result}" height="500px"> <br><h6>${result}</h6></br>` + zxy;
+  document.write(web);
+}
+function asupan(){
+  var data = [
+"http://sansekai.my.id/ptl_repost/120416207_674661289840975_7238538460676645249_n.mp4",
+"http://sansekai.my.id/ptl_repost/120443017_225738842307010_1507614018538871668_n.mp4",
+"http://sansekai.my.id/ptl_repost/120506710_129730895516659_9090102890235113922_n.mp4",
+"http://sansekai.my.id/ptl_repost/120518115_3382156938497131_2702539154432231938_n.mp4",
+"http://sansekai.my.id/ptl_repost/120533450_988960268238482_4908300175960396265_n.mp4",
+"http://sansekai.my.id/ptl_repost/120554993_913582535835353_7937155730001219945_n.mp4",
+"http://sansekai.my.id/ptl_repost/120565745_258483202099028_1831534734126408497_n.mp4",
+"http://sansekai.my.id/ptl_repost/120570166_2716242608644571_5562452335611050430_n.mp4",
+"http://sansekai.my.id/ptl_repost/120571358_3743879342322868_4731152347084614368_n.mp4",
+"http://sansekai.my.id/ptl_repost/120571938_198275171805822_8506241533969509004_n.mp4",
+"http://sansekai.my.id/ptl_repost/120573034_128654721967389_1837144340570017830_n.mp4",
+"http://sansekai.my.id/ptl_repost/120574217_2707319792866165_3682328392840010261_n.mp4",
+"http://sansekai.my.id/ptl_repost/120575986_151816433263092_6600262966520398271_n.mp4",
+"http://sansekai.my.id/ptl_repost/120582400_339926344091433_2581248581156693603_n.mp4",
+"http://sansekai.my.id/ptl_repost/120583739_1060190921079212_3898520254664507328_n.mp4",
+"http://sansekai.my.id/ptl_repost/120587415_638776546998307_3091093882267818607_n.mp4",
+"http://sansekai.my.id/ptl_repost/120589771_752670855329266_5064573607465519463_n.mp4",
+"http://sansekai.my.id/ptl_repost/120613860_3393110177444352_4287165838359264124_n.mp4",
+"http://sansekai.my.id/ptl_repost/120614859_768632723701773_5662521620734316774_n.mp4",
+"http://sansekai.my.id/ptl_repost/120615019_149392973357031_6254963333779779708_n.mp4",
+"http://sansekai.my.id/ptl_repost/120664457_338629710563119_6615226849280369453_n.mp4",
+"http://sansekai.my.id/ptl_repost/120670762_191033325874671_8168246094200167609_n.mp4",
+"http://sansekai.my.id/ptl_repost/120678112_971693299972883_1648826221504742220_n.mp4",
+"http://sansekai.my.id/ptl_repost/120682363_1181866688854694_4233466354892721595_n.mp4",
+"http://sansekai.my.id/ptl_repost/120698858_1302470566751609_3736999773452225729_n.mp4",
+"http://sansekai.my.id/ptl_repost/120707049_337981777304231_3152650741169851669_n.mp4",
+"http://sansekai.my.id/ptl_repost/120714412_680680702806959_2149499366562729814_n.mp4",
+"http://sansekai.my.id/ptl_repost/120678112_971693299972883_1648826221504742220_n.mp4",
+"http://sansekai.my.id/ptl_repost/120682363_1181866688854694_4233466354892721595_n.mp4",
+"http://sansekai.my.id/ptl_repost/120698858_1302470566751609_3736999773452225729_n.mp4",
+"https://telegra.ph/file/1288daecae3d91f8f0839.mp4"
+];
+  var x = Math.floor(Math.random()*data.length);
+  var result = data[x];
+  var web = "<center><h1>Asupan</h1><br>" + `<video autoplay loop controls height="250px">
+  <source src="${result}">
+  </video> <br><h6>${result}</h6></br>` + zxy;
+  document.write(web);
+}
+function absurd(){
+  var data = [
+" Nggak ada yang peduli denganmu di sosmed kecuali kamu cakep.",
+" Sesimpel ini deh, sibuk itu palsu, semua tergantung prioritas.",
+" Dia hanya menghargaimu, bukan mencintaimu.",
+" Keadilan sosial hanya berlaku bagi warna negara yang good looking.",
+" Jangan jadi pelangi untuk orang yang buta warna.",
+" Dia yang tertidur nyenyak setelah mematahkan hatimu tidak pantas untuk kamu ingat.",
+" Dia cuman bercanda, harusnya kamu ketawa, bukan malah jatuh cinta.",
+" Mencintaimu adalah seni menyakiti diri.",
+" Jika tidak bisa mewarnai hidup seseorang, maka jangan pudarkan warna aslinya",
+" Cukup tahu namaku, jangan rupaku",
+" Sesuatu akan terasa berharga jika sudah kehilangan.",
+" Jangan pernah mengeluh ketika kopimu dingin, ia pernah hangat, namun kau diamkan."
+];
+  var x = Math.floor(Math.random()*data.length);
+  var result = data[x];
+  var web = "<center><h1>Kata-kata Absurd</h1><br>" + `<h6>${result}</h6>` + zxy;
+  document.write(web);
+}
+function hack(){
+  var data = [
+"Dear kamu yang tertulis di halaman defacementku, Kapan jadi pacarku?",
+"Aku rela ko jadi Processor yg kepanasan, asalkan kmu yg jadi heatsink'y yg setiap saat bisa mendinginkan ku.",
+"Gak usah nyari celah xss deh, karena ketika kamu ngeklik hatiku udah muncul pop up namamu.",
+"berharap setelah aku berhasil login di hati kamu ga akan ada tombol logout, dan sessionku ga bakal pernah expired.",
+"Masa aku harus pake teknik symlink bypass buat buka-buka folder hatimu yg open_basedir enabled.",
+"Diriku dan Dirimu itu ibarat PHP dan MySQL yang belum terkoneksi.",
+"Jangan cuma bisa inject hatinya,tapi harus bisa patchnya juga. Biar tidak selingkuh sama hacker lain.",
+"Aku memang programmer PHP,tapi aku nggak akan php-in kamu kok.",
+"Eneeeng. | Apache? | Km wanita yg paling Unix yg pernah aku kenal |",
+"Sayang, capslock kamu nyala ya? | ngga, kenapa emangnya? | soalnya nama kamu ketulis gede bgt di hati aku | zzz! smile",
+"Aku deketin kamu cuma untuk redirect ke hati temenmu.",
+"Domain aja bisa parkir, masa cintaku ga bisa parkir dihatimu?",
+"Aku boleh jadi pacarmu? | 400(Bad Request) | Aku cium boleh? | 401(Authorization Required) | Aku buka bajumu yah | 402(Payment Required) sad",
+"kamu tau ga beda'y kamu sama sintax PHP, kalo sintax PHP itu susah di hafalin kalo kamu itu susah di lupain",
+"Kamu dulu sekolah SMK ambil kejuruan apa? | Teknik Komputer Jaringan | Terus sekarang bisa apa aja? | Menjaring hatimu lewat komputerku | biggrin",
+"Jika cinta itu Array, maka,cintaku padamu tak pernah empty jika di unset().",
+"SQLI ( Structured Query Love Injection )",
+"aku ingin kamu rm -rf kan semua mantan di otak mu,akulah root hati kamu",
+"Senyumu bagaikan cooler yang menyejukan hatiku ketika sedang overclock.",
+"kamu adalah terminalku, dimana aku menghabiskan waktuku untuk mengetikan beribu baris kode cinta untukmu smile",
+"Aku seneng nongkrong di zone-h, karena disanalah aku arsipkan beberapa website yang ada foto kamunya.",
+"hatiku ibarat vps hanya untukmu saja bukan shared hosting yg bisa tumpuk berbagai domain cinta.",
+"Aku bukanlah VNC Server Tanpa Authentication yg bisa kamu pantau kapan saja.",
+"Jangan men-dualboot-kan hatiku kepadamu.",
+"cintaku kan ku Ctrl+A lalu kan ku Ctrl+C dan kan ku Ctrl+V tepat di folder system hatimu.",
+"KDE kalah Cantiknya, GNOME kalah Simplenya, FluxBox kalah Ringannya, pokonya Semua DE itu Kalah Sama Kamu.",
+"Cintamu bagaikan TeamViewer yang selalu mengendalikan hatiku",
+"cinta kita tak akan bisa dipisahkan walau setebal apapun itu firewall...!!"
+];
+  var x = Math.floor(Math.random()*data.length);
+  var result = data[x];
+  var web = "<center><h1>Hacker Bucin</h1><br>" + `<h6>${result}</h6>` + zxy;
+  document.write(web);
+}
+function quotes(){
+  var data = [
+"-Kehilangan adalah rencana semesta agar manusia belajar bijaksana<br><br><br>Quotes By:rdlvy",
+"-bukan aku yg menyesal kehilangammu, tapi kmu yg akan menyesal kehilangan orang sebaik aku<br><br>Quotes By Zeev",
+"-aku tak pernah menyesal mencintaimu tpi yg aku sesali adalah terlalu berharap kau adalah takdir nyatanya kau hanya hadir<br><br>Quotes By Zeev",
+"-lepaskan apa yg bukan milikmu, ikhlaskan apa yg sudah tidak lagi ditempatnya, merelakan pertanyaan yg ga akan pernah ada jawabannya, menerima bahwa kadang ga semua orang  bisa bikin kamu bahagia<br><br>Quotes By Zeev",
+"-jika memang komitmen, dia takkan pernah berubah kepadamu. semakin lama hubungan dan semakin tau keadaan bukan semakin bosan tetapi semakin sayang dan mau menerima kekurangan, bukan justru semakin hilang dan banyak alasan<br><br>Quotes By Zeev",
+"-dan sekarang kita berjalan dijalan kita sendiri. saatnya saya mengarungi lautan, menghadapi pasang surut dan angin saya akan mencari alasan lain untuk hidup, kamu menghancurkanku tapi aku akan berusaha baik baik saja tanpamu<br><br>Quotes By Zeev",
+"-aku tidak memaksamu bertahan, jika aku tak membahagiakan saja silahkan<br><br>Quotes By Zeev",
+"-hatiku pernah mati berkali kali, dihujam oleh orang orang terbaik yg dulu pernah memupuk arti dan janji, lalu meninggalkan bila sudah tak berarti<br><br>Quotes By unkow",
+"-tetap tumbuh walau dunia membunuh, tetap melangkah meski jejak sudah kehilangan arah<br><br>Quotes By Zeev",
+"-kenapa masih dia? karna sampai saat ini saya belum mencintai seseorng sekuat saya mencintai dia<br><br>Quotes By Farhan",
+"-aku bukan hanya takut kehilanganmu, tapi aku takut kamu berpura pura mencintaiku<br><br>Quotes By ~Iriene",
+"-patah hatinya ke satu orang mati rasanya ke semua orang<br><br> Quotes By Zeevn",
+"-Sebaik apapun kamu, akan tetap salah dimata orng yg salah. tetapi jika seseorang benar mencintaimu, sebanyak apapun kekuranganmu akan tetap dianggap terbaik olehnya<br><br>Quotes By ~Iriene",
+"-khalil gibran pernah berkata jika kamu mencintai seseorang, biarkan ia pergi. kalau ia kembali berarti ia adalah milikmu kalau ia tidak kembali, maka ia memang tidak pernah menjadi milikmu",
+"-kita jalani ini apa adanya. tak mengikat tak terikat, kita hidup bersama jika tidak sama sama kita terima<br><br>Quotes By ~farhan",
+"-dia yg bilang takut ditinggalkan justru malah dia yg meninggalkan<br><br>Quotes By ~farhan",
+"-kamu beruntung jika sudah menemukan seseorang yg tidak pernah bosan untuk mengerti segala sikapmu yg terkadang menyebalkan dan menjengkelkan<br><br>Quotes By ~farhan",
+"-tidak ada yg bisa menjamin dia berubah meskipun kamu banyak berkorban. tidak ada yg dapat menjamin dia berhenti menyakiti meskipun kamu sudah menerima dan memaafkannya berkali kali. karena perubahan itu datangnya dari diri sendiri<br><br>Quotes By Iriene",
+"-anda kehilangan saya?, saya sudah lebih dulu kehilangan anda bukan saya yg berubah tapi anda<br><br>Quotes By ~Iriene",
+"-jangan karena kamu tau dia begitu mencintaimu kamu jadi seenaknya. jangan karna kamu tau dia selalu memaafkan kesalahanmu kamu terus berulah melakukan kesalahan yg sama. ingat jika dia sudah sampai di titik lelah kemudian memutuskan pergi mungkin untukmu dia tidak memberi kesempatan lagi. maka selagi memiliki jaga dengan baik dan sepenuh hati<br><br>Quotes By ~Iriene",
+"-terkadang meski kita sudah ikhlas melepaskan seseorang yg dicintai ada saat saat tertntu kita mulai mengingatnya lagi membayangkan kejadian yg membuat dada kita terasa sakit lagi. sebab yg namanya luka tidak pernah bisa sembuh dengan sempurna tetap akan ada bekas disana bagaimanapun kita menutupi dan mengobatinya<br><br>Quotes By ~Iriene",
+"-apa bukti bahwa dia mencintai kita dengan tulus? ketika kamu sudah mematahkan hatinya dia masih tetap berkeinginan untuk membahagiakanmu dan dia tidak akan mudah untuk pergi meninggalkanmu<br><br>Quotes By ~Iriene",
+"-semacam diberi harapan namun tidak diprioritaskan. semacam dipertahankan namun tidak diperhatikan<br><br>Quotes By ~Iriene",
+"-pertama wanita akan memperlakukanmu sebagaimna dia ingin diperlakukan. lalu wanita akan memperlakukanmu sebagaimana kamu memperlakukan mereka<br><br>Quotes By ~Iriene",
+"-hilangku tidak dicari hadirku tak dinanti pergiku tak ditahan kembaliku tak diharapkan. ternyata mencintai sendirian rasanya menyakitkan<br><br>Quotes By ~farhan",
+"-perempuan pasti akan cemburu ketika melihat lelakinya dekat dengan perempuan lain meski dia temen sekalipun. munafik jika perempuan itu mengatakan dia baik² saja<br><br>Quotes By ~Iriene",
+"-bohong itu hak kamu dan tugasku hanya pura² diam<br><br>Quotes By Farhan",
+"-kalo bosen bilang jangan diem, biar aku pergi sendiri tanpa kamu suruh<br><br>Quotes By Farhan",
+"-aku tau bahwa kamu berpura² mencintaiku<br><br>Quotes By Farhan",
+"-terkadang sebuah janji adalah kalimat penenang yang berujung sebuah kebohongan<br><br>Quotes By Irene",
+"-yang dulunya berjuang mati matian sekarang belajar untuk melupakan<br><br>Quotes By Zeevn",
+"-dulu kamu meyembuhkan mati rasaku, namun sekarang kamu hadirkan kembali rasa yang kamu hilangkan dulu<br><br>Quotes By Zeevn",
+"-pernah berjuang namun dipatahkan oleh keadaan<br><br>",
+"-Kegagalan itu ibarat tanda koma dalam sebuah kalimat, ia hanya membuat tanda jeda bukan tanda henti<br><br>Quotes By Iriene",
+"-tidak ada yang menyakitimu kamu hanya tersakiti oleh harapanmu",
+"-mungkin lebih baik begini aku takut merusak suasana jika harus mengatakan apa yg harus kurasakan",
+"-jadilah seperti senja yg kedatangannya membuat ketenangan dan kepergian nya membuat kerinduan",
+"-jangan perdulikan aku, pergilah bersamanya, aku suda biasa menikmati luka di akhir cerita",
+"-dia hanya memberi kenyamanan, bukan kepastian",
+"-dilupakan di saat senang, diperlukan di saat kesepian",
+"-semenjak rasa sakit sering menghancurkanku senyumku hanya sebatas palsu",
+"-aku hanya mengikhlaskanmu, bukan berarti aku melupakanmu",
+"-wanita diciptakan istimewa, tetap tegar meski nyaris menyerah, tetap sabar meski ingin mengeluh, dan tetap kuat meski hampir jatuh",
+"- aku akan terus berusaha menjalani hidupku kembali, karena dunia tidak akan berhenti walau setelah engkau pergi",
+"- tahun sudah berganti hari demi hari suda terlewati, tapi kenapa hati ini masih mencintai kamu yang sudah lama pergi",
+"- wanita diciptakan istimewa, tetap tegar meski nyaris menyerah, tetap sabar meski ingin mengeluh, dan tetap kuat meski hampir jatuh"
+];
+  var x = Math.floor(Math.random()*data.length);
+  var result = data[x];
+  var web = "<center><h1>Quotes</h1><br>" + `<h6>${result}</h6>` + zxy;
+  document.write(web);
+}
+
+
+
+
+
+//=====================System Jam=====================//
+var jam = setInterval(function(){
+  var time = new Date();
+  
+  var hour = time.getHours();
+  var minute = time.getMinutes();
+  var second = time.getSeconds();
+  
+  document.getElementById("jam").innerHTML = hour;
+  document.getElementById("menit").innerHTML = minute;
+  document.getElementById("detik").innerHTML = second;
+  
+  if (hour >= 1 && hour <= 10){
+    document.getElementById("selamat").innerHTML = "Selamat Pagi";
+  } else if (hour >= 10 && hour <= 15){
+    document.getElementById("selamat").innerHTML = "Selamat Siang";
+  } else if (hour <= 15 && hour <= 18){
+    document.getElementById("selamat").innerHTML = "Selamat Sore";
+  } else if (hour <= 15 || hour || 1){
+    document.getElementById("selamat").innerHTML = "Selamat Malam";
+  } else {
+    document.getElementById("selamat").innerHTML = "Tengah Malam"
+  }
+  
+},1000);
